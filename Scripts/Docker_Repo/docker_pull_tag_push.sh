0ccfab43e89defd0ef3ec33178dbefa948fc22d9
huseyinahmetaydin <huseyin103@gmail.com>
2018-12-18 11:41:54 +0300

    imagename and version info added

--- Scripts/Docker_Repo/docker_pull_tag_push.sh
+++ Scripts/Docker_Repo/docker_pull_tag_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Docker image:"
+echo "Enter [docker_imagen_name]:[version] (Like : xosproject/volt-synchronizer:2.1.14)  :"
 read d
 docker pull $d
 docker tag $d 192.168.31.203:5000/$d
